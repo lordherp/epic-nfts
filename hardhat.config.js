f915399628ef0043c799cd93d562f047d87a0188
lordherp <sajib.info@gmail.com>
2021-12-16 21:28:20 +0600

    Committed NFT to rinkeby testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,13 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners();
-
-    for (const account of accounts) {
-        console.log(account.address);
-    }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-    solidity: "0.8.0",
+    solidity: '0.8.0',
+    networks: {
+        rinkeby: {
+            url: 'https://eth-rinkeby.alchemyapi.io/v2/jMj9_6ysHW-CHc4MXHyPluU6lmHAVbOd',
+            accounts: ['92bafa46895b2aab1a205ec66407f13a531c8deca1f02be3eda5783729c13001'],
+        },
+    },
 };
+
+
