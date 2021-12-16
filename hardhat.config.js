3ca72f02128a60e224f7c5aed3abe02f93cfa3e8
lordherp <sajib.info@gmail.com>
2021-12-16 21:29:26 +0600

    Added modifications

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
     networks: {
         rinkeby: {
             url: 'https://eth-rinkeby.alchemyapi.io/v2/jMj9_6ysHW-CHc4MXHyPluU6lmHAVbOd',
-            accounts: ['92bafa46895b2aab1a205ec66407f13a531c8deca1f02be3eda5783729c13001'],
+            accounts: [''],
         },
     },
 };
