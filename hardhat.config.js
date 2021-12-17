91026ca273baf5cbd8a9d01c3e69b5cd386a3cab
lordherp <sajib.info@gmail.com>
2021-12-17 19:36:14 +0600

    Fully Functional contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,13 @@
 require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
 
 module.exports = {
     solidity: '0.8.0',
     networks: {
         rinkeby: {
-            url: 'https://eth-rinkeby.alchemyapi.io/v2/jMj9_6ysHW-CHc4MXHyPluU6lmHAVbOd',
-            accounts: [''],
+            url: process.env.STAGING_ALCHEMY_KEY,
+            accounts: [process.env.PRIVATE_KEY],
         },
     },
 };
-
 
